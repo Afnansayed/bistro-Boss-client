ddb1c2115d970745c26c388b3fe9399fa6be3632
Afnansayed <afnansayed1973@gmail.com>
2024-05-28 21:50:56 +0600

    Header setup in axiosSecure instead of indivizual  allUsers

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,6 +4,15 @@ export const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function (config){
+        const token = localStorage.getItem('access-token');
+        console.log("request stop by interceptors",token);
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      });
     return  axiosSecure;
 };
 
